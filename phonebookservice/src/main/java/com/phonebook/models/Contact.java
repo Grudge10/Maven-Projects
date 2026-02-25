d3c6fd5354a0b448007b0956458733916c306438
Jan Liam Arias <100700937+Grudge10@users.noreply.github.com>
2026-02-25 11:05:22 +0000

    implement Contact class with constructors, getters, setters, and CSV methods; update PhonebookService to handle contact addition, search, removal, and CSV operations

--- phonebookservice/src/main/java/com/phonebook/models/Contact.java
+++ phonebookservice/src/main/java/com/phonebook/models/Contact.java
@@ -3,6 +3,17 @@ package com.phonebook.models;
 public class Contact {
     private String name, phoneNumber, email;
 
+    // empty constructor
+    public Contact() {
+    }
+
+    // constructor with all fields as paremeters
+    public Contact(String name, String phoneNumber, String email) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
     // getter methods
     public String getName() {
         return name;
@@ -16,6 +27,20 @@ public class Contact {
         return email;
     }
 
+    // setter methods
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    // method that returns a String formatted for a CSV
     public String toCsvString() {
         return name + "," + phoneNumber + "," + email;
     }
--- phonebookservice/src/main/java/com/phonebook/services/PhonebookService.java
+++ phonebookservice/src/main/java/com/phonebook/services/PhonebookService.java
@@ -8,20 +8,24 @@ import com.phonebook.models.Contact;
 public class PhonebookService {
     private Map<String, Contact> contacts = new HashMap<>();
 
+    // method that adds the contact object into the map
     public void addContact(Contact c) {
         contacts.put(c.getName(), c);
     }
 
-    public boolean searchContact(String name) {
+    // method that searches for the key and returns the searched contact object
+    public Contact searchContact(String name) {
+        // check if key exists. if yes, return contact value
         if (contacts.containsKey(name)) {
-            System.out.println(contacts.get(name));
-            return true;
+            return contacts.get(name);
         } else {
-            return false;
+            return null;
         }
     }
 
+    // method that searches for the key then removes it, and returns boolean value
     public boolean removeContact(String name) {
+        // check if the key exists
         if (contacts.containsKey(name)) {
             contacts.remove(name);
             return true;
@@ -30,25 +34,43 @@ public class PhonebookService {
         }
     }
 
+    // method that saves the hashmap into a csv
     public void saveToCSV(String filename) {
-        try(BufferedWriter writer = new BufferedWriter(new FileWriter(filename))) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filename))) {
+            // loop through all objects
             for (Map.Entry<String, Contact> contact : contacts.entrySet()) {
-                StringBuilder s = new StringBuilder();
+                // get the value of the object from the hashmap and put it into a new instance
+                Contact saveContact = contact.getValue();
 
-                Contact newContact = contact.getValue();
+                // save it into the csv and make a new line for the next one
+                writer.write(saveContact.toCsvString());
+                writer.newLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-                s.append(newContact.toCsvString()).append("\n");
+    // method that loads the data from the csv into the hashmap
+    public void loadFromCSV(String filename) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {
+            String line;
 
-                writer.write(s.toString());
+            // loop through all lines until theres nothing to read
+            while ((line = reader.readLine()) != null) {
+                // split the line
+                String[] data = line.split(",");
+
+                // create a contact object and add in the values
+                Contact loadContact = new Contact(data[0], data[1], data[2]);
+
+                // add the contact into the hashmap
+                contacts.put(data[0], loadContact);
             }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }
-
-    public void loadFromCSV(String filename) {
-
     }
 }
